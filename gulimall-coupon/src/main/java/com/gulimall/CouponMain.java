357586d84b15138a64d1d6171a4668131d0a132d
chenruizhou1 <1259522407@qq.com>
2023-04-25 17:09:15 +0800

    C

---
+++ gulimall-coupon/src/main/java/com/gulimall/CouponMain.java
@@ -0,0 +1,13 @@
+package com.gulimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponMain.class, args);
+    }
+
+}
